f0bdf5e585be971bde6b5e12423f4915d4108369
Jose Carlo San Pedro <josecarlosanpedro0302@gmail.com>
2018-12-04 15:41:21 +0800

    import files

---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// import { Button } from 'antd'
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  
+  render() { 
+   
+    return (
+      <div className="flex column">
+        hello this is electron
+      </div>
+    )
+  }
+
+} // class Home end
